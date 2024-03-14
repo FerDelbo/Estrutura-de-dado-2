4c9a25fcd8f168036e5cec7a5e94f82ba9b11da1
FerDelbo <fedelbone@gmail.com>
2024-03-14 20:32:58 -0300

    Aula 14/03 pt2

--- ED2/stl/pares.cpp
+++ ED2/stl/pares.cpp
@@ -2,17 +2,26 @@
 #include <vector>
 using namespace std;
 
-void pares(vector<int> vec){
+vector<int>* pares(const vector<int>& vec){
+    vector<int>* aux = new vector<int>();
+
     for(int e : vec){
-        if(e%2 == 0) cout<<e<<", ";
+        if(e%2 == 0){
+            //cout<< e << ", ";
+            aux->push_back(e);
+        }
     }
-    cout<<"\n";
+    return aux;
 }
 
 int main(){
-    vector<int> v = {0, 2, 1, 4, 7};
-
-    pares(v);
+    vector<int> v1 = {0, 2, 1, 4, 7};
+    vector<int>* v2 = pares(v1);
+    for(int i : *v2){
+        cout<< i << ", ";
+    }
+    cout<<"\n";
+    //pares(v);
 
     return 0;
 }
