32e8fc3c333dd15bfec9180cdaa0aa44c3e1bc47
FerDelbo <fedelbone@gmail.com>
2024-03-14 20:15:54 -0300

    Aula14/03 pt1

---
+++ ED2/stl/pares.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void pares(vector<int> vec){
+    for(int e : vec){
+        if(e%2 == 0) cout<<e<<", ";
+    }
+    cout<<"\n";
+}
+
+int main(){
+    vector<int> v = {0, 2, 1, 4, 7};
+
+    pares(v);
+
+    return 0;
+}
