8d56fcc799babb7578e25f0f753adc0feea79c48
FerDelbo <fedelbone@gmail.com>
2024-03-14 21:02:56 -0300

    Aula 14/03 pt3

---
+++ ED2/Recursividade/fatorial.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <stdint.h>
+using namespace std;
+
+uint64_t fat(int n){
+    if(n==1) return 1;
+    return n * fat(n-1);
+}
+
+int main(int argc, char** argv){
+    int n = std::atoi(argv[1]);
+
+    cout << fat(n) << "\n";
+}
